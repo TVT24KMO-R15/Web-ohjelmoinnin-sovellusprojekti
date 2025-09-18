ee5ab7ed4efe34c7540582e848065cdb526bda0d
TeroAsilainen <tero_asilainen@hotmail.com>
2025-09-18 09:59:38 +0300

    basic tmdb api tests

---
+++ backend/tests/index.test.js
@@ -0,0 +1,20 @@
+import { expect } from "chai"
+
+describe("Testing basic backend functionality", () => {
+    it("should get popular movies", async () => {
+        const response = await fetch("http://localhost:3000/api/tmdb/popular")
+        const data = await response.json()
+        expect(response.status).to.equal(200)
+        expect(data).to.be.an("object").that.is.not.empty
+        expect(data["results"][0]).to.include.all.keys(["title", "id"])
+    })
+    
+    it("should find movies with a search term", async () => {
+        const searchWord = "123"
+        const response = await fetch(`http://localhost:3000/api/tmdb/search/${searchWord}`)
+        const data = await response.json()
+        expect(response.status).to.equal(200)
+        expect(data).to.be.an("object").that.is.not.empty
+        expect(data["results"][0]).to.include.all.keys(["title", "id"])
+    })
+})
