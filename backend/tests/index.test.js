e3cb1d347e628f5f2481ecf413f6e9e2f0703a6c
TeroAsilainen <tero_asilainen@hotmail.com>
2025-09-18 10:04:08 +0300

    movie details test added

--- backend/tests/index.test.js
+++ backend/tests/index.test.js
@@ -8,7 +8,7 @@ describe("Testing basic backend functionality", () => {
         expect(data).to.be.an("object").that.is.not.empty
         expect(data["results"][0]).to.include.all.keys(["title", "id"])
     })
-    
+
     it("should find movies with a search term", async () => {
         const searchWord = "123"
         const response = await fetch(`http://localhost:3000/api/tmdb/search/${searchWord}`)
@@ -16,5 +16,14 @@ describe("Testing basic backend functionality", () => {
         expect(response.status).to.equal(200)
         expect(data).to.be.an("object").that.is.not.empty
         expect(data["results"][0]).to.include.all.keys(["title", "id"])
+    })
+
+    it("should get movie details with movie id", async () => {
+        const movieId = "157336-interstellar"
+        const response = await fetch(`http://localhost:3000/api/tmdb/details/${movieId}`)
+        const data = await response.json()
+        expect(response.status).to.equal(200)
+        expect(data).to.be.an("object").that.is.not.empty
+        expect(data).to.include.all.keys(["title", "id"])
     })
 })
