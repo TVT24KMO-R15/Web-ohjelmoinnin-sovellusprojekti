9118c1597a3ae56b424d025d340b65c76b31f8f4
mintusmaximus <jheikkinen109@gmail.com>
2025-09-21 03:08:42 +0300

    cleanup code

--- backend/controllers/tmdbController.js
+++ backend/controllers/tmdbController.js
@@ -2,80 +2,69 @@ import { getPopularMovies, searchForMovie, getMovieDetails } from "../services/t
 
 const getPage = (req) => {
   if (!req.params.page) {
-    console.log("no req params page found")
+    // console.log("no req params page found")
     return 1
   } else {
-    console.log("req params page found: " , req.params.page)
+    // console.log("req params page found: " , req.params.page)
     return req.params.page
+  }
+}
+
+/**
+ * Calls TMDB Api endpoint and sends back results object from the request
+ * @param {tmdbServiceFunction} fetchFunction function from tmdbService.js
+ * @param {Number} page Page of results to be fetched
+ * @param  {...any} args Optional arguments (movie name)
+ * @returns `result[]` of movies
+ */
+const handlePagination = async (fetchFunction, page, ...args) => {
+  if (page > 1) {
+    // console.log("Getting multiple pages of results")
+    const allResults = []
+    
+    for (let i = 1; i <= page; i++) {
+      // console.log("going through page" , i)
+      const temp = await fetchFunction(...args, i)
+      
+      // check if result is array and .results exists in it
+      if (!Array.isArray(temp?.results) || temp.results.length === 0) {
+        console.log(`Stopped at page ${i}, out of results`)
+        break
+      }
+      
+      allResults.push(...temp.results)
+      
+      // check if current iteration goes past the total_pages from tmdb query
+      if (temp.total_pages && i >= temp.total_pages) {
+        console.log(`Reached total_pages limit`)
+        break
+      }
+    }
+    
+    return { results: allResults }
+  } else {
+    return await fetchFunction(...args, page)
   }
 }
 
 const fetchPopularMovies = async(req, res, next) => {
   const page = getPage(req)
-  console.log("Page received to fetchpopmovies: " , page)
+  // console.log("Page received to fetchpopmovies: " , page)
 
   try {
-    // if getting more than 1 page 
-    if(page > 1) {
-      console.log("Getting multiple pages of popular movies")
-      // create table for setting all the results from one page into
-      const allResults = []
-      // iterate through page amount, 1--->n
-      for (let i = 1; i <= page; i++) {
-        // set movies to temp var
-        const temp = await getPopularMovies(i)
-        // check if result is array and .results exists in it
-        if (Array.isArray(temp?.results)) {
-          // push results[] into array
-          allResults.push(...temp.results)
-        }
-      }
-      // wrap the entire array of movies inside of results: {} again so frontend can read it 
-      return res.status(200).json({results: allResults})
-      
-    // if not getting multiple pages, business as usual
-    } else {
-      const popularMovies = await getPopularMovies(page)
-      return res.status(200).json(popularMovies)
-    }
+    const popularMovies = await handlePagination(getPopularMovies, page) 
+    return res.status(200).json(popularMovies)
   } catch (err) {
     return next (err)
   }
 }
 
 const searchMovie = async (req, res, next) => {
-  // console.log("searchmovie request " + req)
-  // console.log("searchmovie request params" + req.params)
-  // console.log(req.params.moviename)
   const page = getPage(req)
-  console.log("Searchmovie page: ", page)
   const movieName = (req.params.moviename)
   try {
-    if (page > 1 ) {
-      console.log("Getting multiple pages of search results")
-      const allResults = []
-      for (let i = 1; i <= page; i++) {
-        const temp = await searchForMovie(movieName, i)
-        // if temp has no results array or is empty , stop
-        if (!Array.isArray(temp?.results || temp.results.length === 0)) {
-          console.log(`Stopped movie search at page ${i}, out of results`)
-          break
-        }
-        // set search results from current page to results
-        allResults.push(...temp.results)
-
-        // if reached page limit
-        if (i >= temp.total_pages) {
-          console.log(`Reached total_pages limit in searching`)
-          break
-        }
-      }
-      // return all search results
-      return res.status(200).json({results: allResults})
-    } else {
-      const searchResults = await searchForMovie(movieName, page)
-      return res.status(200).json(searchResults)
-    }
+    const searchResults = await handlePagination(searchForMovie, page, movieName)
+    return res.status(200).json(searchResults)
   } catch (err) {
     return next (err)
   }
