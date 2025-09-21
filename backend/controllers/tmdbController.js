0af42f7f3080b2663d42b1d5fb6789f81e084be3
mintusmaximus <jheikkinen109@gmail.com>
2025-09-21 19:36:41 +0300

    undo backend for smarter implementation

--- backend/controllers/tmdbController.js
+++ backend/controllers/tmdbController.js
@@ -10,49 +10,12 @@ const getPage = (req) => {
   }
 }
 
-/**
- * Calls TMDB Api endpoint and sends back results object from the request
- * @param {tmdbServiceFunction} fetchFunction function from tmdbService.js
- * @param {Number} page Page of results to be fetched
- * @param  {...any} args Optional arguments (movie name)
- * @returns `result[]` of movies
- */
-const handlePagination = async (fetchFunction, page, ...args) => {
-  if (page > 1) {
-    // console.log("Getting multiple pages of results")
-    const allResults = []
-    
-    for (let i = 1; i <= page; i++) {
-      // console.log("going through page" , i)
-      const temp = await fetchFunction(...args, i)
-      
-      // check if result is array and .results exists in it
-      if (!Array.isArray(temp?.results) || temp.results.length === 0) {
-        console.log(`Stopped at page ${i}, out of results`)
-        break
-      }
-      
-      allResults.push(...temp.results)
-      
-      // check if current iteration goes past the total_pages from tmdb query
-      if (temp.total_pages && i >= temp.total_pages) {
-        console.log(`Reached total_pages limit`)
-        break
-      }
-    }
-    
-    return { results: allResults }
-  } else {
-    return await fetchFunction(...args, page)
-  }
-}
-
 const fetchPopularMovies = async(req, res, next) => {
   const page = getPage(req)
   // console.log("Page received to fetchpopmovies: " , page)
 
   try {
-    const popularMovies = await handlePagination(getPopularMovies, page) 
+    const popularMovies = await getPopularMovies(page) 
     return res.status(200).json(popularMovies)
   } catch (err) {
     return next (err)
@@ -63,7 +26,7 @@ const searchMovie = async (req, res, next) => {
   const page = getPage(req)
   const movieName = (req.params.moviename)
   try {
-    const searchResults = await handlePagination(searchForMovie, page, movieName)
+    const searchResults = await searchForMovie(movieName, page)
     return res.status(200).json(searchResults)
   } catch (err) {
     return next (err)
