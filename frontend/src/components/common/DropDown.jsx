0972c9d607a8d8c9778404118e5d2659215f01f6
mintusmaximus <jheikkinen109@gmail.com>
2025-09-23 23:23:41 +0300

    Moviesearch can access dropdown menu items

--- frontend/src/components/common/DropDown.jsx
+++ frontend/src/components/common/DropDown.jsx
@@ -1,33 +1,34 @@
 import React from 'react'
+import '../../global.css'
 import './DropDown.css'
 
 
-
-const handleChange = (e) => {
-    console.log(e.target.value)
-}
 
 /**
  * Creates a new HTML `<select>` element, with `<option>`'s
  * @param Title What is shown next to the dropdown menu button 
  * @param Label gets matched from the text to the selector
- *  
+ * 
  */
-export default function DropDown({title, label, items}) {
-  return (
-  <>
-    <div id='dropDownHolder'>
-      <label html={label}>
-        {title}
-      </label>
+// todo fill that out later ^
+export default function DropDown({title, label, items, onSelect, selected}) {
 
-      <select name={label} id="todo something here later" onChange={handleChange}>
+const handleChange = (e) => {
+  // console.log(e.target.value)
+  onSelect(e.target.value) // <- call function inside parent
+}
+
+  return (
+    <div id='dropDownHolder'>
+      <label html={label} id='textlabel'>{title}</label>
+      <select name={label} id='dropDown' onChange={handleChange} value={selected}>
         <option value='' >Choose an option...</option>
-        {items.map(item => (
-          <option value={item}>{item}</option>
+        {items.map((item, index) => (
+          <option key={index} value={item}>
+            {item}
+          </option>
         ))}
       </select>
     </div>
-  </>
   )
 }
--- frontend/src/pages/MovieSearch.jsx
+++ frontend/src/pages/MovieSearch.jsx
@@ -5,25 +5,30 @@ import PopularMovies from '../components/home/PopularMovies';
 import DropDown from '../components/common/DropDown';
 import './MovieSearch.css'
 
-// note to self maybe add recursive props to reuse this as advanced search, props tweaking url parameters (or whatever tmdb uses for advanced search)
-// ...from dropdown menus and such
-// or maybe make advanced options into its own prop for reuse elsewhere (group search refinement, review search refinement??)
 export default function MovieSearch() {
-  // when loading /search/ without going through a searchbar the value of e is null and causes errors, this is a workaround
-  const locationValue = useLocation();
+  const locationValue = useLocation(); // when loading /search/ without going through a searchbar the value of e is null and causes errors, this is a workaround
   const movieName = locationValue.state && locationValue.state.e ? locationValue.state.e : null; // if entering search via url, set moviename to null and then the search doesnt get called
   const h2Name = movieName ? "Search results" : "" // h2 header text for movies listing
   const [open, setOpen] = useState(false) // toggle for showing advanced search results
+  const [selections, setSelections] = useState({}) // JSON object of stored selections
 
   // if moviename exists (received via useLocation when searching, but not on direct /search load), set url with moviename
-  const url = movieName ? `http://localhost:3000/api/tmdb/search/${movieName}` : null 
-  const changeOpen = () => {
-    setOpen(!open)
+  const url = movieName ? `http://localhost:3000/api/tmdb/search/${movieName}` : null // todo update this url later
+  const changeOpen = () => setOpen(!open) // toggles filter dropdowns visibility
+
+  const dropDownData = {
+    // todo change these to TMDB parameters later
+    year: [2021, 2022, 2023, 2024, 2025],
+    language: ["Something", "something else", "yet some more"],
+    genre: ["firstgenre", "second", "youknowwho"]
   }
-  const things = []
-  for (let i = 0; i < 10; i ++)
-  {
-    things.push(i)
+
+  const handleSelect = (key, value) => {
+    setSelections((prev) => ({
+      ...prev,
+      [key]: value,
+    }))
+    console.log(`MOVIESEARCH: Dropdown ${key} selected: ${value}`)
   }
 
   return (
@@ -31,18 +36,43 @@ export default function MovieSearch() {
     <div id='movieSearchBar'> 
       <h1>Movie search</h1>
       <SearchBar searchDestination={"/search"}/> 
+
         <div id='dropDownHolder'>
-          <button onClick={changeOpen}>{open? "Close advanced filtering" : "Advanced filtering"}</button>
-          { open ? 
-              <div id='selectors'>
-                <DropDown title={'Filter by something'} label={'dd1'} items={things}/>
-                <DropDown title={'Filter by something else'} label={'dd2'} items={things}/>
-                {/* <DropDown title={'Choose another filter'} label={''}/> */}
-              </div> 
-            : <span/> // show nothing on toggle off
-          }
+          <button onClick={changeOpen} id='filterToggle'>
+            {open? "Close filters" : "Open filters"}
+          </button>
+          
+          { open ? (
+            <div id='selectors'>
+
+              <DropDown
+                title={'Release year'}
+                label={'year'}
+                items={dropDownData.year}
+                onSelect={(val) => handleSelect("year", val)}
+                selected={selections.year || ""}
+              />
+
+              <DropDown
+                title={'Original language'}
+                label={'lang'}
+                items={dropDownData.language}
+                onSelect={(val) => handleSelect("language", val)}
+                selected={selections.language || ""}
+              />
+
+              <DropDown
+                title={'Something else'}
+                label={'genre'}
+                items={dropDownData.genre}
+                onSelect={(val) => handleSelect("genre", val)}
+                selected={selections.genre || ""}
+              />
+            </div>
+          ) : (<span/> // show nothing on toggle off
+          )}
+          </div>
         </div>
-    </div>
       <PopularMovies reqUrl={url} sectionTitle={h2Name} />
     </>
   )
