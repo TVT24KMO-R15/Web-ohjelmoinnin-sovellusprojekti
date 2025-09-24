240f8b06449ae64d8d16956dbaea267986e0701a
t3koee03 <t3koee03@students.oamk.fi>
2025-09-24 18:54:17 +0300

    NavBar nyt muuttuu jos kirjautuu sisään

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -3,15 +3,22 @@ import './Navbar.css'
 import Authentication from './Authentication';
 import { Link } from 'react-router-dom';
 import SearchBar from './SearchBar';
-
+import {useUser} from '../../context/UseUser';
 
 export default function Navbar() {
-  const closeIcon = (<svg xmlns="http://www.w3.org/2000/svg" className="close-btn" height="24px" viewBox="0 -960 960 960" width="24px" fill="#ffffffff"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>);
-  const menuIcon = (<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#ffffffff"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>);
+  const closeIcon = (<svg xmlns="http://www.w3.org/2000/svg" className="close-btn" height="24px" viewBox="0 -960 960 960" width="24px" fill="#fff"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>);
+  const menuIcon = (<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>);
         
   const [sidebarOpen, setSidebarOpen] = useState(false); // Sivuvalikko, joka on käytössä vain kapeille näytöille
   const [signinOpen, setSigninOpen] = useState(false); // Kirjautumisikkuna
+  const { user } = useUser();
 
+  /* Sulje kirjautuminen kun on vastaanotettu user token */
+  React.useEffect(() => {
+    if (user && user.token) {
+      setSigninOpen(false);
+    }
+  }, [user]);
   return (
     <>
     {/* Sivuvalikko */}
@@ -23,7 +30,13 @@ export default function Navbar() {
           <li><Link to="/search">Movies</Link></li>
           <li><Link to="/groups">Groups</Link></li>
           <li><Link to="/reviews">Reviews</Link></li>
-          <li className="Sign_in" onClick={() => setSigninOpen(true)}>Sign in</li>
+          { user && user.token ? (
+            <li><Link to="#">Sign out</Link></li>
+          ) : ''}
+          
+          { !user || !user.token ? (
+            <li onClick={() => setSigninOpen(true)}><Link to="">Sign in</Link></li>
+          ) : '' }
         </ul>
         <ul>
           {/* Normaali valikko */}
@@ -37,18 +50,27 @@ export default function Navbar() {
           {/* Venyvä hakupainike */}
               <SearchBar searchDestination={'/search'}/>
 
-          {/* Sisäänkirjautumisnappi */}
-          <button className="signin-btn"
-            onClick={() => setSigninOpen(true)}>
+        {/* Sisäänkirjautumisnappi */}
+        { !user || !user.token ? (
+          <button className="signin-btn" onClick={() => setSigninOpen(true)}>
             Sign in
           </button>
+          
+        ) : (
+          <>
+          {/* Käyttäjänappi */}
+            <button className="signin-btn" onClick={() => setSidebarOpen(true)}>
+              Account
+            </button>
+          </>
+        )}
           </ul>
           </nav>
       
           {/* Näytä modal vain jos signinOpen === true */}
-          {signinOpen && <Authentication onClose={() => setSigninOpen(false)} />}
+        {signinOpen && <Authentication onClose={() => setSigninOpen(false)} />}
 
-   
+        
 
     </>
   )
