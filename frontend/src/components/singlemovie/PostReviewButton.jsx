c5f4cc696a9ca366c8d4746739ff3ae2015eeb82
TeroAsilainen <tero_asilainen@hotmail.com>
2025-09-30 20:37:02 +0300

    bug fix

--- frontend/src/components/singlemovie/PostReviewButton.jsx
+++ frontend/src/components/singlemovie/PostReviewButton.jsx
@@ -16,26 +16,29 @@ export default function PostReviewButton({ onUpdate }) {
     const account = useUser()
 
     useEffect(() => {
-        try {
-            axios.get(import.meta.env.VITE_API_URL + `/reviews/movieuser/${movieId}/${account.user.id}`)
-                .then(response => {
-                    console.log(response)
-                    if (response.data.length == 0) {
-                        setAllowReview(true)
-                    } else {
-                        setAllowReview(false)
+        if (account.user.username) {
+            console.log(account)
+            try {
+                axios.get(import.meta.env.VITE_API_URL + `/reviews/movieuser/${movieId}/${account.user.id}`)
+                    .then(response => {
+                        console.log(response)
+                        if (response.data.length == 0) {
+                            setAllowReview(true)
+                        } else {
+                            setAllowReview(false)
+                        }
                     }
-                }
-                ).catch(err => {
-                    console.error('Failed to fetch reviews', err)
+                    ).catch(err => {
+                        console.error('Failed to fetch reviews', err)
 
-                })
-                .finally(() => {
-                    setLoading(false)
-                })
-        } catch (error) {
-            console.error('Failed to fetch reviews', err)
-        }
+                    })
+                    .finally(() => {
+                        setLoading(false)
+                    })
+            } catch (error) {
+                console.error('Failed to fetch reviews', err)
+            }
+        } setLoading(false)
     }, [reloadButton])
 
 
@@ -43,7 +46,7 @@ export default function PostReviewButton({ onUpdate }) {
     if (!allowReview) {
         return <>{(account.user.id) ? <div>
             <button className='review-button' onClick={() => setPostReviewOpen(true)}>Edit Your Own Review</button>
-            {postReviewOpen && (<PostReview  onClose={() => { setPostReviewOpen(false); }} property={'put'} onUpdate={() => { onUpdate();}} reload={() => {setReloadButton(!reloadButton);}} />)}
+            {postReviewOpen && (<PostReview onClose={() => { setPostReviewOpen(false); }} property={'put'} onUpdate={() => { onUpdate(); }} reload={() => { setReloadButton(!reloadButton); }} />)}
         </div> : <p>Log in to post your own review</p>}</>
 
     }
@@ -51,7 +54,7 @@ export default function PostReviewButton({ onUpdate }) {
         return (
             <div>
                 <button className='review-button' onClick={() => setPostReviewOpen(true)}>Post Your Own Review</button>
-                {postReviewOpen && (<PostReview  onClose={() => { setPostReviewOpen(false); }} property={'post'} onUpdate={() => { onUpdate();}} reload={() => {setReloadButton(!reloadButton);}} />)}
+                {postReviewOpen && (<PostReview onClose={() => { setPostReviewOpen(false); }} property={'post'} onUpdate={() => { onUpdate(); }} reload={() => { setReloadButton(!reloadButton); }} />)}
             </div>
         )
     }
