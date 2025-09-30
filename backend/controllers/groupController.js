b12141395c95190f4b369c4799628f7d7f94d0b4
t3koee03 <t3koee03@students.oamk.fi>
2025-09-30 03:09:11 +0300

    savepoint

--- backend/controllers/groupController.js
+++ backend/controllers/groupController.js
@@ -1,1 +1,77 @@
 // for all group table http endpoints
+import { queryAllGroups, queryAllGroupsWithLimit, queryPostGroup, queryDeleteGroup, queryUpdateGroup } from "../models/group.js";
+
+const postGroup= async (req, res, next) => {
+    const { groups } = req.body
+    try {
+        if (!groups.fk_ownerid || !groups.groupname || !groups.groupdescription) {
+            const error = new Error('Missing Group data')
+            error.status = 400
+            return next(error)
+        }
+
+        const result = await queryPostGroup(groups.fk_ownerid, groups.groupname, groups.groupdescription)
+        return res.status(201).json(result)
+    } catch (error) {
+        return next(error)
+    }
+}
+
+const updateGroup= async (req, res, next) => {
+    const { groups } = req.body
+    try {
+        if (!groups.groupid || !groups.fk_ownerid || !groups.groupname || !groups.groupdescription) {
+            const error = new Error('Missing Group update data')
+            error.status = 400
+            return next(error)
+        }
+
+        const result = await queryUpdateGroup(groups.groupid, groups.fk_ownerid, groups.groupname, groups.groupdescription)
+        return res.status(201).json(result)
+    } catch (error) {
+        return next(error)
+    }
+}
+
+const getAllGroups = async (req, res, next) => {
+    try {
+        const result = await queryAllGroups()
+        console.log("get all Groups")
+        return res.status(200).json(result.rows)
+    } catch (error) {
+        return next (error) // send error to middleware in index.js
+    }
+}
+
+const getAllGroupsWithLimit = async (req, res, next) => {
+    try {
+        
+        const result =  await queryAllGroupsWithLimit(req.params.limit)
+        console.log("get all Groups with limit "+ req.params.limit)
+        return res.status(200).json(result.rows)
+    } catch (error) {
+        return next (error)
+    }
+}
+
+const deleteGroup = async (req, res, next) => {
+    const { id } = req.params
+
+    try {
+        console.log(`Deleting Group with id: ${id}`)
+        const result = await queryDeleteGroup(id)
+        if (result.rowCount === 0) {
+            const error = new Error('Group not found')
+            error.status = 400
+            return next(error)
+        }
+        return res.status(200).json({ id: id })
+    } catch (error) {
+        return next(error)
+    }
+}
+export{
+    postGroup, getAllGroups, getAllGroupsWithLimit, updateGroup, deleteGroup
+}
+
+
--- backend/models/group.js
+++ backend/models/group.js
@@ -1,1 +1,31 @@
 // for all group table sql queries
+
+import { pool } from "../helpers/dbHelper.js"
+
+const queryPostGroup = async (fk_ownerid, groupname, groupdescription) => {
+    return await pool.query(`INSERT INTO groups (fk_ownerid, groupname, groupdescription) VALUES ($1, $2, $3) RETURNING *`,
+        [fk_ownerid, groupname, groupdescription]
+    )
+}
+
+const queryAllGroups = async () => {
+    return await pool.query(`SELECT * FROM group ORDER BY groupname DESC`)
+}
+
+const queryAllGroupsWithLimit = async ( limit ) => {
+    return await pool.query(`SELECT * FROM groups ORDER BY groupname DESC LIMIT $1`, [limit])
+}
+
+const queryUpdateGroup = async (groupid, fk_ownerid, groupname, groupdescription)=> {
+    return await pool.query(`UPDATE groups
+SET fk_ownerid = $2 groupname = $3 groupdescription = $4
+WHERE groupid = $1;) VALUES ($1, $2, $3, $4) RETURNING *`,
+        [groupid, fk_ownerid, groupname, groupdescription]
+    )
+}
+
+const queryDeleteGroup = async (groupid) => {
+    return await pool.query(`DELETE FROM groups WHERE groupid = $1`, [groupid])
+}
+
+export { queryAllGroups, queryAllGroupsWithLimit, queryPostGroup, queryDeleteGroup, queryUpdateGroup }
