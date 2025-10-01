6e625e623ffb5629807aa12550279ec34f8ea10e
t3koee03 <t3koee03@students.oamk.fi>
2025-10-01 03:58:31 +0300

    Rest client ja auth middleware

--- backend/routes/groupRouter.js
+++ backend/routes/groupRouter.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
-
+import { auth } from '../helpers/authHelper.js'
 import { getAllGroups, getGroupById, postGroup, deleteGroup, updateGroup } from "../controllers/groupController.js"
 
 const router = Router()
 
 router.get('/', getAllGroups)
 router.get('/:id', getGroupById)
-router.post('/post', postGroup)
-router.delete('/delete', deleteGroup)
-router.put('/update', updateGroup)
+router.post('/post', auth, postGroup)
+router.delete('/delete', auth, deleteGroup)
+router.put('/update', auth, updateGroup)
 
 export default router
