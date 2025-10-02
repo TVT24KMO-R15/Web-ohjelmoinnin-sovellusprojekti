896bca85925837f53c63a8c732cc83f4692f1336
TeroAsilainen <tero_asilainen@hotmail.com>
2025-10-02 14:43:16 +0300

    Groups Search Backend

--- backend/models/group.js
+++ backend/models/group.js
@@ -16,6 +16,10 @@ const queryGroupById = async (id) => {
     return await pool.query(`SELECT * FROM "groups" WHERE groupid = $1`, [id])
 }
 
+const queryGroupBySearchWord = async (word) => {
+    return await pool.query(`SELECT * FROM groups WHERE groupname ILIKE $1`, ['%' + word + '%'])
+}
+
 const queryUpdateGroup = async (groupid, ownerid, groupname, groupdescription)=> {
     return await pool.query(`UPDATE "groups"
 SET fk_ownerid = $2, groupname = $3, groupdescription = $4
@@ -27,4 +31,4 @@ const queryDeleteGroup = async (groupid, ownerid) => {
     return await pool.query(`DELETE FROM groups WHERE groupid=$1 AND fk_ownerid=$2 RETURNING *`, [groupid, ownerid])
 }
 
-export { queryAllGroups, queryGroupById, queryPostGroup, queryDeleteGroup, queryUpdateGroup }
+export { queryAllGroups, queryGroupById, queryGroupBySearchWord, queryPostGroup, queryDeleteGroup, queryUpdateGroup }
