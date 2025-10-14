adcb2662dd4e1799842a8e51d78b356b8b8f39be
t3koee03 <t3koee03@students.oamk.fi>
2025-10-14 21:07:36 +0300

    noPoster jos ei posteria

--- frontend/src/components/groups/FavouriteMovieSelector.jsx
+++ frontend/src/components/groups/FavouriteMovieSelector.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import { useUser } from '../../context/UseUser';
 import './FavouriteMovieSelector.css'
-
+import noPoster from '../../assets/noPoster.png';
 export default function FavouriteMovieSelector({ onMovieSelect, isVisible }) {
     const { user } = useUser();
     const [favourites, setFavourites] = useState([]);
@@ -46,13 +46,7 @@ export default function FavouriteMovieSelector({ onMovieSelect, isVisible }) {
             {favourites.length > 0 ? (
               favourites.map((movie) => (
                 <div className="movie-info">
-                  <img
-                    src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
-                    alt={movie.title}
-                    className="fav-poster"
-                  />
-                    <p><strong>{movie.title}</strong></p>
-                    <div>
+                   <div>
                      <button
                   type="button"
                   onClick={() => handleSelectMovie(movie.id)}
@@ -61,6 +55,18 @@ export default function FavouriteMovieSelector({ onMovieSelect, isVisible }) {
                   Select Movie
                 </button>
                  </div>
+                  {movie.poster_path ? (
+                    <img
+                      src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+                      alt={movie.title}
+                      className="fav-poster"
+                    />
+                  ) : (
+                    <img src={noPoster} alt="No Poster" className="fav-poster" />
+                  )}
+                  
+                    <p><strong>{movie.title}</strong></p>
+                   
                  </div> 
               ))
             ) : (
